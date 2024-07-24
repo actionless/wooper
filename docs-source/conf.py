382fb322ed753ec51c335b24a6ee487ed1bb069d
actionless <actionless.loveless@gmail.com>
2024-07-24 08:53:19 +0200

    doc(conf): don't hardcode year

--- docs-source/conf.py
+++ docs-source/conf.py
@@ -15,6 +15,7 @@
 
 import sys
 import os
+from datetime import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -51,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'wooper'
-copyright = '2015-2020, actionless'
+copyright = '2015-' + str(datetime.now().year) + ', actionless'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
